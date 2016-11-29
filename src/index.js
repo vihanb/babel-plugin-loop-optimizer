3de27289c46bdad93c68ab662de52a4a61f72a71
Vihan <vihanb@users.noreply.github.com>
2016-11-28 19:47:53 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,113 @@
+const FUNCS = ["map", "forEach"];
+
+function checkName(path) {
+  return path.node.callee.property && FUNCS.indexOf(path.node.callee.property.name) > -1;
+}
+
+export default function (babel) {
+  const { types: t } = babel;
+  
+  return {
+    visitor: {
+      CallExpression(path) {
+        var parent = path.getStatementParent();
+        if (checkName(path)) {
+          var name = path.node.callee.property.name;
+          
+          var arrayName = path.scope.generateUidIdentifier("a");
+          var funcName = path.scope.generateUidIdentifier("f");
+          var resArrName = path.scope.generateUidIdentifier("r");
+          
+          var iterator = path.scope.generateUidIdentifier("i");
+
+          var call = t.callExpression(
+            funcName,
+            [
+              t.memberExpression(
+                arrayName,
+                iterator,
+                true
+              ),
+              iterator,
+              arrayName
+            ]
+          );
+          
+          var expr = t.callExpression(
+            t.memberExpression(
+              t.memberExpression(
+                arrayName,
+              	iterator,
+              	true
+              ),
+              t.identifier("push")
+            ),
+            [ call ]
+          );
+          
+          path.getStatementParent().insertBefore([
+            t.variableDeclaration(
+              "var",
+              [
+                t.variableDeclarator(
+                  arrayName,
+                  path.node.callee.object
+                )
+              ]
+            ),
+            
+            t.variableDeclaration(
+              "var",
+              [
+                t.variableDeclarator(
+                  funcName,
+                  path.node.arguments[0]
+                )
+              ]
+            ),
+            
+            t.variableDeclaration(
+              "var",
+              [
+                t.variableDeclarator(
+                  resArrName,
+                  t.arrayExpression()
+                )
+              ]
+            ),
+            
+            t.forStatement(
+              t.variableDeclaration(
+                "var",
+                [
+                  t.variableDeclarator(
+                    iterator,
+                    t.numericLiteral(0)
+                  )
+                ]
+              ),
+              t.binaryExpression(
+                '<',
+                iterator,
+                t.memberExpression(
+                  arrayName,
+                  t.identifier('length')
+                )
+              ),
+              t.updateExpression(
+                '++',
+                iterator
+              ),
+              t.expressionStatement(
+                name === "forEach" ? call : expr
+              )
+            )
+          ]);
+          
+          path.replaceWith(resArrName);
+        }
+      }
+    }
+  };
+}
+
