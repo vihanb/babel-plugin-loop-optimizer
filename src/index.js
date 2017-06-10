9244776a9474704def743031680e4647bde7955d
Vihan B <contact@vihan.org>
2017-06-09 19:58:11 -0700

    Fixed bug
    Signed-off-by: Vihan B <contact@vihan.org>

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ export default function (babel) {
   const { types: t } = babel;
   
   return {
-    name: "ast-transform", // not required
     visitor: {
       IfStatement(path) {
         if (!t.isBlockStatement(path.node.consequent)) {
