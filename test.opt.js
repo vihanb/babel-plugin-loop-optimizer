0a1085a0551959baa0c42e1c20e62a4ef6e189ad
Nikita <MoofMonkey@users.noreply.github.com>
2019-01-09 13:02:21 +0300

    Fixed test.opt.js, upgraded babel back

--- test.opt.js
+++ test.opt.js
@@ -1,32 +1,30 @@
 {
-    let _a = console.log("FORWARD")
+    console.log("FORWARD");
+    let ar = [1, 2, 3];
     // loop-optimizer: FORWARD
-    [(1, 2, 3)];
+    let _a = ar;
     let _f = console.log;
 
-    for (let _i = _a.length; _i--;) {
+    for (let _i = 0; _i < _a.length; _i++) {
         _f(_a[_i], _i, _a);
     }
 }
 
 {
-    let _a2 = console.log("KEEP")
+    console.log("KEEP");
+    let ar = [1, 2, 3];
     // loop-optimizer: KEEP
-    [(1, 2, 3)];
+    ar.forEach(console.log);
+}
+
+{
+    console.log("nothing");
+    let ar = [1, 2, 3];
+    // nothing
+    let _a2 = ar;
     let _f2 = console.log;
 
     for (let _i2 = _a2.length; _i2--;) {
         _f2(_a2[_i2], _i2, _a2);
-    }
-}
-
-{
-    let _a3 = console.log("nothing")
-    // nothing
-    [(1, 2, 3)];
-    let _f3 = console.log;
-
-    for (let _i3 = _a3.length; _i3--;) {
-        _f3(_a3[_i3], _i3, _a3);
     }
 }
